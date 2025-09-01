90670b82707da68b388628bf55aaabc9ae8d4f6b
Eoinker <138410167+Eoinker@users.noreply.github.com>
2025-09-01 13:41:30 -0400

    Connected PlayerJump to platforms toggling on and off

--- Assets/Scripts/makeInvisible.cs
+++ Assets/Scripts/makeInvisible.cs
@@ -3,34 +3,57 @@ using UnityEngine;
 
 public class makeInvisible : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool isOn;
+
+    public void Awake()
     {
-        shiftPhase();
+        if (this.gameObject.tag == "GroupAPlatforms")
+        {
+            TurnOff();
+        }
+        else
+        {
+            TurnOn();
+        }
+    }
+
+    public void OnEnable()
+    {
+        PlayerMovement.playerJumped += shiftPhase;
+    }
+
+    public void OnDisable()
+    {
+        PlayerMovement.playerJumped -= shiftPhase;
     }
 
     void shiftPhase()
     {
-        if (GetComponent<BoxCollider2D>().enabled == true)
+        if (isOn)
         {
-            Color color = GetComponent<Renderer>().material.color;
-            color.a = 0.3f;
-            GetComponent<Renderer>().material.color = color;
-            GetComponent<BoxCollider2D>().enabled = false;
+            TurnOff();
         }
         else
         {
-            Color color = GetComponent<Renderer>().material.color;
-            color.a = 1f;
-            GetComponent<Renderer>().material.color = color;
-            GetComponent<BoxCollider2D>().enabled = true;
+            TurnOn();
         }
     }
 
-
-    // Update is called once per frame
-    void Update()
+    public void TurnOn()
     {
-        
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 1f;
+        GetComponent<Renderer>().material.color = color;
+        GetComponent<BoxCollider2D>().enabled = true;
+        isOn = true;
+    }
+
+    public void TurnOff()
+    {
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 0.3f;
+        GetComponent<Renderer>().material.color = color;
+        GetComponent<BoxCollider2D>().enabled = false;
+        isOn = false;
     }
 }
