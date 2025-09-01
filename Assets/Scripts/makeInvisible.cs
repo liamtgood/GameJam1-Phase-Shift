7723323ed2d82b19162982002bcd897b31e101ab
liamtgood <liamgood83@gmail.com>
2025-09-01 12:03:34 -0400

    added a scirpt to make tiles opacity decrease and remove the colider

---
+++ Assets/Scripts/makeInvisible.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class makeInvisible : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        shiftPhase();
+    }
+
+    void shiftPhase()
+    {
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 0.3f;
+        GetComponent<Renderer>().material.color = color;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
