30e2beddc394c0aba9fb17503980202799bf09dc
liamtgood <liamgood83@gmail.com>
2025-09-01 21:31:53 -0400

    player and tiles work

--- Assets/MoveTilesToNewLocation.cs
+++ Assets/MoveTilesToNewLocation.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class MoveTilesToNewLocation : MonoBehaviour
 {
+    public GameObject tilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,10 +13,17 @@ public class MoveTilesToNewLocation : MonoBehaviour
         //if a tiles Y value is 10 less than the camera y value then
         //move the tiles Y value to +20 the camera value
         int randomX = Random.Range(-6, 6);
+        int numberOfTiles = Random.Range(2, 5);
         if (transform.position.y < Camera.main.transform.position.y - 10)
         {
-            transform.position = new Vector3(Camera.main.transform.position.x + randomX, Camera.main.transform.position.y + 30, transform.position.z);
+
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                Instantiate(tilePrefab, new Vector3(randomX + i * 5, Camera.main.transform.position.y + 20, transform.position.z), Quaternion.identity);
+            }
+            Destroy(gameObject);
         }
+
        
     }
 }
