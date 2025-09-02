8befa31abcd5b445add32c4347d1270bb4f05677
Samantha Groner <114039769+Sasheanna@users.noreply.github.com>
2025-09-01 22:26:13 -0400

    Basic menu in place

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] PlayerMovement playerMvmt;
+
+    // enable/disable player movement when this is called
+    public void setPlayerCanMove(bool canMove)
+    {
+        playerMvmt.enabled = canMove;
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("You have quit the game");
+        Application.Quit();
+    }
+}
